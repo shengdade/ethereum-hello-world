6c9342b27b28c4880670b70265e5c16603a40976
Dade Sheng <dasheng@amazon.com>
2021-08-29 22:31:06 -0400

    Submitting your Smart Contract to Etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@
 
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
@@ -16,5 +17,8 @@ module.exports = {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
